939c607674eff77a7b1a8f6bfa8fba328b4a2d0d
RajAppcleus <bibin.raj@appcleus.in>
2016-12-16 12:06:20 +0530

    application

---
+++ config/webpack/environments/development.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+  module.exports = function(_path) {
+  return {
+  context: _path,
+  debug: true,
+  devtool: 'source-map',
+  output: {
+   publicPath: '/'
+  },
+  devServer: {
+  contentBase: './dist',
+  info: false,
+  inline: false,
+  host:"0.0.0.0",
+   port:3000
+  }
+ };
+};
